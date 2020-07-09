6ca3478d84a983665576edac576befd41e7d489c
estabravo <jbravo.post@gmail.com>
2020-07-08 23:41:49 -0700

    querying for new message element

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+const squares = document.querySelectorAll(".square");
+const colorDisplay = document.getElementById('colorDisplay');
+const messageDisplay = document.getElementById('message');
+let pickedColor = colors[3];
+
 let colors = [
     "rgb(255, 0, 0)",
     "rgb(255, 255, 0)",
@@ -6,10 +11,6 @@ let colors = [
     "rgb(0, 0, 255)",
     "rgb(255, 0, 255)"
 ]
-
-const squares = document.querySelectorAll(".square");
-let pickedColor = colors[3];
-const colorDisplay = document.getElementById('colorDisplay'); 
 
 colorDisplay.textContent = pickedColor; 
 
