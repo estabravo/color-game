26bb76477a8be445f7d324b66367280605de408d
estabravo <jbravo.post@gmail.com>
2020-07-08 23:01:22 -0700

    linked colorDisplay to show pickedColor in dom

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ const squares = document.querySelectorAll(".square");
 let pickedColor = colors[3];
 const colorDisplay = document.getElementById('colorDisplay'); 
 
+colorDisplay.textContent = pickedColor; 
+
 for (let i = 0; i < squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
 };
